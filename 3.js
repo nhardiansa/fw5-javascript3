7045c46872c20df8e0eceec2339213c968cc3cf4
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-20 16:53:44 +0800

    melengkapi tugas 3

--- 3.js
+++ 3.js
@@ -6,6 +6,11 @@ const coffee = [
     price: 2000,
     brewTime: 3000
   },
+  {
+    brand: 'kapal api',
+    price: 3000,
+    brewTime: 2000
+  },
 ]
 
 const buatKopi = (brand, money) => {
@@ -13,26 +18,60 @@ const buatKopi = (brand, money) => {
 
     const coffeeCheck = coffee.find(item => item.brand === brand)
     let changeMoney = money - coffeeCheck.price
-    
+
     if (coffeeCheck === undefined) {
       reject('Brand tidak ditemukan')
-    }
-
-    if (changeMoney < 0) {
+    } else if (changeMoney < 0) {
       reject('Uang anda tidak cukup')
+    } else {
+      setTimeout(() => {
+        if (changeMoney > 0) {
+          resolve(`Kopi ${coffeeCheck.brand} siap dihidangkan dan ini ${changeMoney} untuk kembaliannya`)
+        }
+        resolve(`Kopi ${coffeeCheck.brand} siap dihidangkan`)
+      }, coffeeCheck.brewTime)
     }
 
-    console.log('Kopi sedang disiapkan');
-
-    setTimeout(() => {
-      if (changeMoney > 0) {
-        resolve(`Kopi ${coffeeCheck.brand} siap dihidangkan dan ini ${changeMoney} untuk kembaliannya`)
-      }
-      resolve(`Kopi ${coffeeCheck.brand} siap dihidangkan`)
-    }, coffeeCheck.brewTime)
   })
 }
 
-buatKopi('torabika', 1000)
-  .then(result => console.log(result))
-  .catch(error => console.log(error))
+buatKopi('kapal api', 3000)
+  .then(result => {
+    console.log(result)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
+
+
+
+  /*=============== beli pulsa =============== */
+  
+const beliPulsa = (credit, money) => {
+  return new Promise((resolve, reject) => {
+    const change = money - credit
+
+    if (change < 1000) {
+      reject('Saldo anda tidak mencukupi')
+    } else {
+      setTimeout(() => {
+        resolve('Pembayaran berhasil')
+      }, 2000)
+    }
+  })
+}
+
+const mauBeliPulsa = async (credit, money) => {
+  try {
+    console.log('Sedang melakukan pembayaran...');
+    const result = await beliPulsa(credit, money)
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('Transaksi selesai');
+  }
+} 
+
+mauBeliPulsa(5000, 5500)
